1e89ee7a1c4167194831f43a645560c7c003dd88
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-04 20:21:03 +0500

    Footer Created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaFacebook, FaGithub } from "react-icons/fa"
+import {} from "react-icons/gr"
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='bg-blue-950'>
+        <div className="container mx-auto p-3 text-center text-lg text-white">
+            <p>Copyright&copy; Allright reserved 2024 <span className="text-yellow-300">GH URL SHORTER</span> </p>
+            <div className="flex items-center gap-4 justify-center">
+              
+                <Link href={'/gh'} className='text-xl text-yellow-300 font-bold'>
+                    <FaFacebook />
+                </Link>
+              
+                <Link href={'/gh'} className='text-xl text-yellow-300 font-bold'>
+                    <FaGithub />
+                </Link>
+              
+                <Link href={'/gh'} className='text-xl text-yellow-300 font-bold'>
+                <FaGithub />
+                </Link>
+
+
+
+
+
+            </div>
+        </div>
+    </footer>
   )
 }
 
