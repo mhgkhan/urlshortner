cd2a1aa18546f1535c51b6e564d9961a780bd421
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-06 22:57:28 +0500

    Change Heading and Paragraph for HERO COMPONENT

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero = () => {
     return (
         <section className='hero'>
-            <h1 className="md:text-5xl text-3xl text-blue-500 font-bold text-center my-3"> Effortlessly Shorten URLs with Our User-friendly Tool </h1>
+            <h1 className="md:text-5xl text-3xl text-blue-500 font-bold text-center my-3"> Trim Your Links with <span className="text-yellow-500">GH URL SHORTNER</span></h1>
             <p className="text-lg text-center font-bold text-blue-800">
-                Quickly shorten long URLs with our intuitive URL shortener. Simplify your links and enhance user experience seamlessly.
+            Simplify and share with our lightning-fast URL shortener. Perfect for optimizing social media posts, email campaigns, and more. Experience seamless redirects and insightful analytics—all in one click!
             </p>
         </section>
     )
