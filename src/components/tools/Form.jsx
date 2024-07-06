27d72fc1160a54df70f2992cf8db79ec888fc3e6
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-06 23:37:21 +0500

    Created and Added Onchange Function on Input

--- src/components/tools/Form.jsx
+++ src/components/tools/Form.jsx
@@ -15,7 +15,6 @@ const Form = () => {
     const [isPermissionOk,setIsPermissionOk] = useState(false)
     // function for paste last copy text 
     const pasteCopied = async e => {
-
         try {   
             const clipBoardData = await navigator.clipboard.readText()
             setInput(clipBoardData)
@@ -28,6 +27,10 @@ const Form = () => {
         }
     }
 
+
+    // function to change data in the input 
+    const changeInput = e => setInput(e.target.value);
+
     const submitUrlForm = e => {
         e.preventDefault()
     }
@@ -39,7 +42,7 @@ const Form = () => {
 
             <form onSubmit={submitUrlForm} className='w-full flex items-center justify-center gap-1 md:flex-row flex-col'>
                 <div className=" flex items-center justify-between gap-1 input border border-1 border-blue-600 rounded-md md:w-[75%] w-full p-1 hover:shadow-md hover:shadow-gray-200">
-                    <input type="url" value={input} name='url' id='url' placeholder='Enter Url' className=' placeholder:font-bold text-lg px-2 w-[80%] border-none outline-none py-2 ' />
+                    <input type="url" value={input} onChange={changeInput} name='url' id='url' placeholder='Enter Url' className=' placeholder:font-bold text-lg px-2 w-[80%] border-none outline-none py-2 ' />
                     <span onClick={pasteCopied} className='cursor-pointer active:scale-105 text-3xl text-blue-900 px-2 hover:text-yellow-500'><FaPaste /></span>
                 </div>
                 <button className='py-3 px-2 active:scale-95 text-xl bg-blue-950 text-white font-bold outline-none border border-1 border-blue-800 rounded-lg  hover:bg-white hover:text-yellow-600 hover:border-blue-700 w-full md:w-auto md:m-0 my-2'>SHORT NOW</button>
