0571a9935159696aa5436a866512e87734df18c3
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-06 22:51:38 +0500

    CUSTOMIZE RESPONSIVES OF FORM

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
     return (
         <section className='hero'>
-            <h1 className="text-5xl text-blue-500 font-bold text-center my-3"> Effortlessly Shorten URLs with Our User-friendly Tool </h1>
+            <h1 className="md:text-5xl text-3xl text-blue-500 font-bold text-center my-3"> Effortlessly Shorten URLs with Our User-friendly Tool </h1>
             <p className="text-lg text-center font-bold text-blue-800">
                 Quickly shorten long URLs with our intuitive URL shortener. Simplify your links and enhance user experience seamlessly.
             </p>
