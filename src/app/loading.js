82948723d2e4b543c8b01b4463c2ba16affb7edc
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-06 23:15:59 +0500

    Add Main Tag to Loading Page

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,9 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <Loading />
+    <main className='min-h-screen flex items-center justify-center'>
+        <Loading />
+    </main>
   )
 }
 
