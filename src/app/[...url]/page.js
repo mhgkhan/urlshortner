169034b0a5429677211e81b956fb35fedc1d3271
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-08 22:03:19 +0500

    Changes in Redirect (url) page

--- src/app/[...url]/page.js
+++ src/app/[...url]/page.js
@@ -4,25 +4,30 @@ import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 import React from "react";
 
-
 const page = async ({ params }) => {
-  
-  // console.log(params)
-  const { url } = params;
-  if(url!==null && url!==undefined){
-    const checkUrlId = await UrlStr.findOne({urlId:url[0]})
-    if(checkUrlId  && checkUrlId!==null){
-      console.log(checkUrlId)
-      redirect(checkUrlId.originalUrl)
+  try {
+    // console.log(params)
+    const { url } = params;
+    if (url !== null && url !== undefined) {
+      const checkUrlId = await UrlStr.findOne({ urlId: url[0] });
+      if (checkUrlId && checkUrlId !== null) {
+        console.log(checkUrlId);
+        redirect(checkUrlId.originalUrl);
+      } else {
+        redirect("/");
+      }
+    } else {
+      redirect("/");
     }
-    else{
-      redirect("/")
-    }
+  } catch (error) {
+    return (
+      <main>
+        <div className="container mx-auto">
+          <Loading />
+        </div>
+      </main>
+    );
   }
-  else{
-    redirect("/")
-  }
-
 
   // console.log(url[0])
   // const URL = process.env.DOMAIN + url + "/";
@@ -41,13 +46,13 @@ const page = async ({ params }) => {
   //   return;
   // }
 
-  return (
-    <main>
-      <div className="container mx-auto">
-        <Loading />
-      </div>
-    </main>
-  );
+  // return (
+  //   <main>
+  //     <div className="container mx-auto">
+  //       <Loading />
+  //     </div>
+  //   </main>
+  // );
 };
 
 export default page;
