b2d1bc6d3d2b586fdac9b74acc097021da874233
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-12 20:05:35 +0500

    Added Error Resposne

---
+++ src/app/api/urls/searchurl/[...url]/route.js
@@ -0,0 +1,10 @@
+import { sendResponse } from "@/utils/utilityFunctions";
+
+
+export async function GET(request, content){
+    try {
+        
+    } catch (error) {
+        return sendResponse(false,"Some went wrong please try again later", null)
+    }
+}
