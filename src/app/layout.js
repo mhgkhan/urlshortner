0dea40ae8c7c99a0bc755bba9284e455e65ff8b9
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-17 09:17:41 +0500

    Added Sitemap.xml and Changes in Layout metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,17 @@ export const metadata = {
   title: "GH UrlShortner",
   description: "GH UrlShortner is a powerful and user-friendly URL shortening service that transforms long links into concise, 8-character URLs. Our platform ensures fast, reliable, and secure link shortening for easy sharing and improved accessibility. Simplify your links and enhance your digital presence with GH UrlShortner today! Built with cutting-edge technologies like Next.js and styled with TailwindCSS.",
   keywords: "URL shortener, link shortener, shorten URLs, 8-character URLs, URL shortening service, fast URL shortener, secure URL shortener, URL management, link sharing, GH UrlShortner",
-  author: "mhgkhan"
+  author: "mhgkhan",
+  url: "https://usgh.netlify.app",
+  image: "https://avatars.githubusercontent.com/u/128809509?v=4",
+  type: "website",
+  site_name: "GH UrlShortner",
+  icons: {
+    icon: ["/favicon.ico"],
+    apple: ["/apple-touch-icon.png"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
