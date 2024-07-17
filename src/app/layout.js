58726e23b54f60b5e67b6ee0409511fcf0cf2a66
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-17 09:27:58 +0500

    Added Google Search Console Meta Tag for verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -50,6 +50,8 @@ export default function RootLayout({ children }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#b36db6" />
         <meta name="theme-color" content="#ffffff" />
+        
+        <meta name="google-site-verification" content="x9YuyIlhUHD9CsateViRxBhr5jAlA7i_TmndcM15By8" />
       </head>
       <body className={`${GlassAntiqua.className} bg-gray-100`}>
         <Header />
