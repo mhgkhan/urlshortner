9739cca78ff2a3a875f429cc8e2354d5e482f570
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-24 13:05:35 +0500

    Rename Title of the webstie

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,8 @@ export const NewRoker = New_Rocker({ subsets: ["latin"], weight: ["400"] });
 const GlassAntiqua = Glass_Antiqua({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
-  title: "Url Shortify || urlshortify.com",
-  description: "url Shortify is a powerful and user-friendly URL shortening service that transforms long links into concise, 8-character URLs. Our platform ensures fast, reliable, and secure link shortening for easy sharing and improved accessibility. Simplify your links and enhance your digital presence with GH UrlShortner today! Built with cutting-edge technologies like Next.js and styled with TailwindCSS.",
+  title: "GH UrlShortner || usgh.netlify.app",
+  description: "GH Urlshortner is a powerful and user-friendly URL shortening service that transforms long links into concise, 8-character URLs. Our platform ensures fast, reliable, and secure link shortening for easy sharing and improved accessibility. Simplify your links and enhance your digital presence with GH UrlShortner today! Built with cutting-edge technologies like Next.js and styled with TailwindCSS.",
   keywords: "URL shortener, link shortener, shorten URLs, 8-character URLs, URL shortening service, fast URL shortener, secure URL shortener, URL management, link sharing, GH UrlShortner",
   author: "mhgkhan",
   url: "https://usgh.netlify.app",
