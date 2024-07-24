fe93cdcbd65b500565ac7e4d7290a30cbe8aba41
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-24 13:16:22 +0500

    Added Meta data for about page

---
+++ src/app/about/layout.js
@@ -0,0 +1,11 @@
+
+export const metadata = {
+    title:"About || usgh.com",
+    description:"Discover GH UrlShortner: a powerful and efficient URL shortening service dedicated to transforming long links into concise, 8-character URLs. Learn about our mission to provide fast, reliable, and secure link shortening, our commitment to enhancing your digital presence, and the cutting-edge technologies we use to deliver top-notch service. Join us in simplifying your online experience.",
+    keywords:"about GH UrlShortner, URL shortener, link shortening service, 8-character URLs, fast and reliable URL shortener, secure URL shortener, digital presence, simplify links, URL shortening technology",
+
+}
+
+export default async function aboutLayout({children}){
+    return{children}
+}
